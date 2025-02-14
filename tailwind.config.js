b5d6b8784cd5e2a7cfab4602577983e345576531
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-02-14 02:18:15 +0100

    Feat: header beginning

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Stardos Stencil", "sans-serif"],
+        paragraph: ["Rokkitt", "sans-serif"],
+      },
+      colors: {
+        brand: {
+          banner: "#335C67",
+          main: "#E09F3E",
+          cta: "#9E2A2B",
+        },
+      },
+    },
   },
   plugins: [],
 }
