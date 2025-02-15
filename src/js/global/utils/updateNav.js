bfe5da5dfb8696a9118fb8be7ae9491bdb93c530
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-02-15 16:52:25 +0100

    Feat: hamburger functionality and logout button hidden

---
+++ src/js/global/utils/updateNav.js
@@ -0,0 +1,42 @@
+export function updateNav() {
+  const username = localStorage.getItem("username");
+
+  const loginLinkDesktop = document.querySelector('a[href="/auth/login/"]');
+  const registerLinkDesktop = document.querySelector('a[href="/auth/register/"]');
+  const loginLinkMobile = document.querySelector('#hamburger-menu a[href="/auth/login/"]');
+  const registerLinkMobile = document.querySelector('#hamburger-menu a[href="/auth/register/"]');
+
+  const logoutButton = document.getElementById("logoutButton");
+  const logoutButtonMobile = document.getElementById("logoutButtonMobile");
+
+  if (username) {
+    loginLinkDesktop.style.display = "none";
+    registerLinkDesktop.style.display = "none";
+    loginLinkMobile.style.display = "none";
+    registerLinkMobile.style.display = "none";
+  } else {
+    loginLinkDesktop.style.display = "block";
+    registerLinkDesktop.style.display = "block";
+    loginLinkMobile.style.display = "block";
+    registerLinkMobile.style.display = "block";
+  }
+
+  if (username) {
+    logoutButton.style.display = "block";
+    logoutButtonMobile.style.display = "block";
+  } else {
+    logoutButton.style.display = "none";
+    logoutButtonMobile.style.display = "none";
+  }
+};
+export function toggleHamburgerMenu() {
+  const hamburgerButton = document.querySelector("#hamburger-button");
+  const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+  hamburgerButton.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("hidden");
+  });
+};
+
+toggleHamburgerMenu();
+updateNav();
