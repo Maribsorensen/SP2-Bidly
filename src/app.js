8d66cb9bab4da72c69d8946ae8f57cc0ddbe3434
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-02-15 17:44:47 +0100

    Feat: started router functions

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import "./css/style.css";
 import { toggleHamburgerMenu, updateNav } from "./js/global/utils/updateNav";
 
-// import router from "./js/router";
+import router from "./js/router";
 
-// await router(window.location.pathname);
+await router(window.location.pathname);
 toggleHamburgerMenu();
 updateNav();
---
+++ src/js/router/index.js
@@ -0,0 +1,15 @@
+export default async function router(pathname = window.location.href) {
+  switch (pathname) {
+    case "/":
+      await import("./views/home.js");
+      break;
+    case "/auth/login/":
+      await import("./views/login.js");
+      break;
+    case "/auth/register/":
+      await import("./views/register.js");
+      break;
+    default:
+      await import("./views/notFound.js");
+  }
+}
