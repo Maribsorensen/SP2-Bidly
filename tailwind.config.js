5297af4eb02ec1354af5d106cc9d8cdf4ce971a1
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-02-15 17:35:25 +0100

    Feat: changed repo to vite. Added css import to app.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./**/*.{html,js}", "!./node_modules/**/*"],
   theme: {
     extend: {
@@ -18,4 +18,3 @@ module.exports = {
   },
   plugins: [],
 }
-
