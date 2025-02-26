15bf94e7f184d23b887bd59e1d3b645fad714f31
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-02-26 15:46:17 +0100

    Feat: update nav to show or not show profile

--- src/js/global/utils/updateNav.js
+++ src/js/global/utils/updateNav.js
@@ -3,15 +3,18 @@ export function updateNav() {
 
   const loginLinks = document.querySelectorAll('a[href="/auth/login/"]');
   const registerLinks = document.querySelectorAll('a[href="/auth/register/"]');
+  const profileLinks = document.querySelectorAll('a[href="/profile/"]');
   const logoutButtons = document.querySelectorAll("#logoutButton, #logoutButtonMobile");
 
   if (username) {
     loginLinks.forEach(link => link.style.display = "none");
     registerLinks.forEach(link => link.style.display = "none");
+    profileLinks.forEach(link => link.style.display = "block");
     logoutButtons.forEach(button => button.style.display = "block");
   } else {
     loginLinks.forEach(link => link.style.display = "block");
     registerLinks.forEach(link => link.style.display = "block");
+    profileLinks.forEach(link => link.style.display = "none");
     logoutButtons.forEach(button => button.style.display = "none");
   }
 }
