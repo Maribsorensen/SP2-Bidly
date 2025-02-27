25c5c6b4238f8e7b311cadfe776cdd4a2da775cf
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-02-27 23:48:34 +0100

    Fix: trying to deploy again

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/SP2-bidly/',
+  base: "",
   build: {
-    target: 'es2022',
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
+        profile: resolve(__dirname, "./profile/index.html"),
+        editProfile: resolve(__dirname, "./profile/edit/index.html"),
+        listing: resolve(__dirname, "./listing/index.html"),
+        editListing: resolve(__dirname, "./listing/edit/index.html"),
+        createListing: resolve(__dirname, "./listing/create/index.html"),
+        singleListing: resolve(__dirname, "./listing/singleListing/index.html"),
+      },
+    },
   },
 });
