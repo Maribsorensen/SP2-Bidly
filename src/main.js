c1a5156f7b8461b587af9d347f8027fa96d954e4
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-02-28 12:19:34 +0100

    Fix: added FA(it was not the issue)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import "./css/style.css";
+import { loadFontAwesome } from "./js/global/icons";
 import router from "./js/router";
 
 
 await router(window.location.pathname);
-
+loadFontAwesome();
 
 
 
